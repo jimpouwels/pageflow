bef67bbc82169ecf3aedb592351a28c21bf1f8d3
Jim Pouwels <jim.pouwels@gmail.com>
2026-09-11 22:00:25 +0200

    Pageflow: image usages

--- src/modules/images/templates/images/image_usage_viewer.tpl
+++ src/modules/images/templates/images/image_usage_viewer.tpl
@@ -1,5 +1,5 @@
 <ul>
     {foreach from=$element_holders item=element_holder}
-        <li><strong>{$element_holder.type}:</strong> {$element_holder.title}</li>
+        <li><strong>{$element_holder.type}:</strong> <a href="{$element_holder.url}" target="_blank">{$element_holder.title}</a></li>
     {/foreach}
 </ul>
--- src/modules/images/visuals/images/ImageUsageViewer.php
+++ src/modules/images/visuals/images/ImageUsageViewer.php
@@ -4,6 +4,9 @@ namespace Pageflow\Core\modules\images\visuals\images;
 
 use Pageflow\Core\view\views\Panel;
 use Pageflow\Core\view\TemplateData;
+use Pageflow\Core\core\BlackBoard;
+use Pageflow\Core\database\dao\ModuleDao;
+use Pageflow\Core\database\dao\ModuleDaoMysql;
 use Pageflow\Core\elements\photo_album_element\dao\PhotoAlbumElementHelperDao;
 use Pageflow\Core\elements\image_element\dao\ImageElementHelperDao;
 use Pageflow\Core\core\model\ElementHolderType;
@@ -12,12 +15,14 @@ class ImageUsageViewer extends Panel {
 
     private PhotoAlbumElementHelperDao $photoAlbumElementHelperDao;
     private ImageElementHelperDao $imageElementHelperDao;
+    private ModuleDao $moduleDao;
     private int $imageId;
 
     public function __construct(int $imageId) {
         parent::__construct($this->getTextResource('image_usage_viewer_title'));
         $this->photoAlbumElementHelperDao = new PhotoAlbumElementHelperDao();
         $this->imageElementHelperDao = new ImageElementHelperDao();
+        $this->moduleDao = ModuleDaoMysql::getInstance();
         $this->imageId = $imageId;
     }
 
@@ -39,20 +44,26 @@ class ImageUsageViewer extends Panel {
                 'id' => $elementHolder->getId(),
                 'name' => $elementHolder->getName()
             );
+            $moduleIdentifier = '';
+            $param = '';
             switch ($elementHolder->getType()) {
                 case ElementHolderType::PAGE:
                     $elementHolderData['type'] = $this->getTextResource('images_usage_viewer_page_type');
-                    $elementHolderData['url'] = ""; // {backendBaseUrlRaw}?module_id=?page module id???
+                    $moduleIdentifier = 'pages';
+                    $param = 'page';
                     break;
                 case ElementHolderType::ARTICLE:
                     $elementHolderData['type'] = $this->getTextResource('images_usage_viewer_article_type');
-                    $elementHolderData['url'] = "";
+                    $moduleIdentifier = 'articles';
+                    $param = 'article';
                     break;
                 case ElementHolderType::BLOCK:
                     $elementHolderData['type'] = $this->getTextResource('images_usage_viewer_block_type');
-                    $elementHolderData['url'] = "";
+                    $moduleIdentifier = 'blocks';
+                    $param = 'block';
                     break;
             }
+            $elementHolderData['url'] = $this->getBackendUrlForElementHolder($moduleIdentifier, $param, $elementHolder->getId());
             $elementHolderData['title'] = $elementHolder->getTitle();
             $elementHoldersData[] = $elementHolderData;
         }
@@ -65,22 +76,36 @@ class ImageUsageViewer extends Panel {
                 'id' => $elementHolder->getId(),
                 'name' => $elementHolder->getName()
             );
+            $moduleIdentifier = '';
+            $param = '';
             switch ($elementHolder->getType()) {
                 case ElementHolderType::PAGE:
-                    $elementHolderData['type'] = 'page';
-                    $elementHolderData['url'] = "";
+                    $elementHolderData['type'] = $this->getTextResource('images_usage_viewer_page_type');
+                    $moduleIdentifier = 'pages';
+                    $param = 'page';
                     break;
                 case ElementHolderType::ARTICLE:
-                    $elementHolderData['type'] = 'article';
-                    $elementHolderData['url'] = "";
+                    $elementHolderData['type'] = $this->getTextResource('images_usage_viewer_article_type');
+                    $moduleIdentifier = 'articles';
+                    $param = 'article';
                     break;
                 case ElementHolderType::BLOCK:
-                    $elementHolderData['type'] = 'block';
-                    $elementHolderData['url'] = "";
+                    $elementHolderData['type'] = $this->getTextResource('images_usage_viewer_block_type');
+                    $moduleIdentifier = 'blocks';
+                    $param = 'block';
                     break;
             }
+            $elementHolderData['url'] = $this->getBackendUrlForElementHolder($moduleIdentifier, $param, $elementHolder->getId());
             $elementHolderData['title'] = $elementHolder->getTitle();
             $elementHoldersData[] = $elementHolderData;
         }
+    }
+
+    private function getBackendUrlForElementHolder(string $moduleIdentifier, string $param, int $elementHolderId): string {
+        $module = $this->moduleDao->getModuleByIdentifier($moduleIdentifier);
+        if (!$module) {
+            return "";
+        }
+        return BlackBoard::getBackendBaseUrlRaw() . "?module_id=" . $module->getId() . "&" . $param . "=" . $elementHolderId;
     }
 }
